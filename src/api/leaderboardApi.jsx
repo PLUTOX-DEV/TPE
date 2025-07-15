ffa72ecfb1c03ac1186721032bc0cbc2396c2e05
Victor <151991623+PLUTOX-DEV@users.noreply.github.com>
2025-07-15 10:17:42 -0700

    Update leaderboardApi.jsx

--- src/api/leaderboardApi.jsx
+++ src/api/leaderboardApi.jsx
@@ -1,6 +1,6 @@
 // api/leaderboardApi.js
 export const getReferralLeaderboard = async () => {
-  const res = await fetch("https://nakabozoz.onrender.com/api/users/referral-leaderboard");
+  const res = await fetch("https://nakabozoz-bn.onrender.com/api/users/referral-leaderboard");
 
   if (!res.ok) throw new Error("Failed to fetch leaderboard");
   return await res.json();
